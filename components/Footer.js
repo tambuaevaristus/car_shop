977d4b8a3e106f2d10d63528be57adff3c39919f
Desmond <desmondtekum9@gmail.com>
2023-04-02 17:51:46 +0100

    Footer adjustment

--- components/Footer.js
+++ components/Footer.js
@@ -12,14 +12,14 @@ export default function Footer() {
             <div class="col-lg-3 col-md-6">
               <h4 class="text-light mb-4">Address</h4>
               <p class="mb-2">
-                <i class="fa fa-map-marker-alt me-3"></i>123 Street, New York,
-                USA
+                <i class="fa fa-map-marker-alt me-3"></i>Molyko, Buea
+                Cameroon
               </p>
               <p class="mb-2">
-                <i class="fa fa-phone-alt me-3"></i>+012 345 67890
+                <i class="fa fa-phone-alt me-3"></i>+237654297746
               </p>
               <p class="mb-2">
-                <i class="fa fa-envelope me-3"></i>info@example.com
+                <i class="fa fa-envelope me-3"></i>topbids@gmail.com
               </p>
               <div class="d-flex pt-2">
                 <a class="btn btn-outline-light btn-social" href="">
@@ -41,29 +41,29 @@ export default function Footer() {
               <h6 class="text-light">Monday - Friday:</h6>
               <p class="mb-4">09.00 AM - 09.00 PM</p>
               <h6 class="text-light">Saturday - Sunday:</h6>
-              <p class="mb-0">09.00 AM - 12.00 PM</p>
+              <p class="mb-0">07.00 AM - 12.00 PM</p>
             </div>
             <div class="col-lg-3 col-md-6">
-              <h4 class="text-light mb-4">Services</h4>
+              <h4 class="text-light mb-4">Brands</h4>
               <a class="btn btn-link" href="">
-                Diagnostic Test
+                 Bughatti Veyron
               </a>
               <a class="btn btn-link" href="">
-                Engine Servicing
+                Toyota
               </a>
               <a class="btn btn-link" href="">
-                Tires Replacement
+                Mercedes bennz
               </a>
               <a class="btn btn-link" href="">
-                Oil Changing
+                Range Rover
               </a>
               <a class="btn btn-link" href="">
-                Vacuam Cleaning
+                Lamborghini
               </a>
             </div>
             <div class="col-lg-3 col-md-6">
               <h4 class="text-light mb-4">Newsletter</h4>
-              <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
+              <p>Please sign up for newsletter.</p>
               <div class="position-relative mx-auto" >
                 <input
                   class="form-control border-0 w-100 py-3 ps-4 pe-5"
@@ -85,22 +85,11 @@ export default function Footer() {
             <div class="row">
               <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
                 &copy;{" "}
-                <a class="border-bottom" href="#">
-                  Your Site Name
+                <a class="border-bottom " href="#">
+                  Topbids
                 </a>
-                , All Right Reserved. Designed By{" "}
-                <a class="border-bottom" href="https://htmlcodex.com">
-                  HTML Codex
-                </a>
-                <br />
-                Distributed By:{" "}
-                <a
-                  class="border-bottom"
-                  href="https://themewagon.com"
-                  target="_blank"
-                >
-                  ThemeWagon
-                </a>
+                   All Right Reserved. Designed By{" Topbids designers "}
+    
               </div>
               <div class="col-md-6 text-center text-md-end">
                 <div class="footer-menu">
