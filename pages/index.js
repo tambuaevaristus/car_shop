91de5d717b9787c34bf5816a62e335c7d2df3700
Tambua Evaristus <evaristustambua@gmail.com>
2023-04-03 13:09:43 +0100

    samm

--- pages/index.js
+++ pages/index.js
@@ -19,30 +19,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Barlow:wght@600;700&family=Ubuntu:wght@400;500&display=swap"
-          rel="stylesheet"
-        />
-
-        <link
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css"
-          rel="stylesheet"
-        />
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css"
-          rel="stylesheet"
-        />
-
-        <link href="lib/animate/animate.min.css" rel="stylesheet" />
-        <link
-          href="lib/owlcarousel/assets/owl.carousel.min.css"
-          rel="stylesheet"
-        />
-        <link
-          href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css"
-          rel="stylesheet"
-        />
+     
       </Head>
       <div>
         <Header />
--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,29 @@
-(function ($) {
-    "use strict";
+// (function ($) {
+//     "use strict";
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
+//     // Spinner
+//     var spinner = function () {
+//         setTimeout(function () {
+//             if ($('#spinner').length > 0) {
+//                 $('#spinner').removeClass('show');
+//             }
+//         }, 1);
+//     };
+//     spinner();
     
     
-    // Initiate the wowjs
-    new WOW().init();
+//     // Initiate the wowjs
+//     new WOW().init();
 
 
-    // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.sticky-top').css('top', '0px');
-        } else {
-            $('.sticky-top').css('top', '-100px');
-        }
-    });
+//     // Sticky Navbar
+//     $(window).scroll(function () {
+//         if ($(this).scrollTop() > 300) {
+//             $('.sticky-top').css('top', '0px');
+//         } else {
+//             $('.sticky-top').css('top', '-100px');
+//         }
+//     });
     
     
     // Dropdown on mouse hover
